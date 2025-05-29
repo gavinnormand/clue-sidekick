d06c5b63ddeea6c4cd7c02b9aa4c8bbb38fa2667
gavinnormand <normandgavin@gmail.com>
2025-05-29 10:13:28 -0700

    Initial Spring Boot Commit

---
+++ cluesidekick_api/src/main/java/com/cluesidekick/cluesidekick_api/CluesidekickApiApplication.java
@@ -0,0 +1,13 @@
+package com.cluesidekick.cluesidekick_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CluesidekickApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CluesidekickApiApplication.class, args);
+	}
+
+}
---
+++ cluesidekick_api/src/test/java/com/cluesidekick/cluesidekick_api/CluesidekickApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cluesidekick.cluesidekick_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CluesidekickApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
