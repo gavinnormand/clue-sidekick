66a248b122095e23e3c60681eadfcb7f46ad503f
gavinnormand <normandgavin@gmail.com>
2025-07-10 15:42:27 -0400

    Update Web Config

--- cluesidekick_api/src/main/java/com/cluesidekick/cluesidekick_api/controller/GameController.java
+++ cluesidekick_api/src/main/java/com/cluesidekick/cluesidekick_api/controller/GameController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = { "http://localhost:5173", "https://cluesidekick.vercel.app" }, allowCredentials = "true")
 public class GameController {
 
     private final GameService gameService;
